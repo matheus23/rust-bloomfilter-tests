18efa08b1d8293cf4776e08f71a53c3b23389f67
Philipp Krüger <philipp.krueger1@gmail.com>
2022-06-15 16:18:42 +0200

    Remove expored alternatives that turned out inferior

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use std::time::Instant;
 
 use blake3;
 use xxhash_rust::xxh3;
-// use xxhash_rust::xxh32;
-// use xxhash_rust::xxh64;
 
 struct Bloom<const N: usize> {
     bytes: [u8; N],
@@ -21,34 +19,6 @@ const LUT: [u8; 8] = [
     0b1000_0000,
 ];
 
-struct EnhancedDoubleHash {
-    x: u32,
-    y: u32,
-    n: u32,
-}
-
-impl EnhancedDoubleHash {
-    fn new(hash1: u32, hash2: u32) -> Self {
-        Self {
-            x: hash1,
-            y: hash2,
-            n: 1,
-        }
-    }
-}
-
-impl Iterator for EnhancedDoubleHash {
-    type Item = u32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.x = self.x.wrapping_add(self.y);
-        self.y = self.y.wrapping_add(self.n);
-        self.n += 1;
-
-        Some(self.x)
-    }
-}
-
 impl<const N: usize> Bloom<N> {
     pub fn new(k_hashes: u64) -> Self {
         Self {
@@ -57,7 +27,6 @@ impl<const N: usize> Bloom<N> {
         }
     }
 
-    #[cfg(not(indices = "edh"))]
     pub fn indices<'a>(&self, element: &'a [u8]) -> impl Iterator<Item = usize> + 'a {
         (0..self.k_hashes).map(|seed| {
             let hash = xxh3::xxh3_64_with_seed(element, seed) as usize;
@@ -65,16 +34,6 @@ impl<const N: usize> Bloom<N> {
             // let hash = xxh32::xxh32(element, seed as u32) as usize;
             hash % (N * 8)
         })
-    }
-
-    #[cfg(indices = "edh")]
-    pub fn indices<'a>(&self, element: &'a [u8]) -> impl Iterator<Item = usize> + 'a {
-        let hash = xxh3::xxh3_64(element);
-        let hash1 = (hash >> 32) as u32;
-        let hash2 = hash as u32;
-        EnhancedDoubleHash::new(hash1, hash2)
-            .take(self.k_hashes as usize)
-            .map(|n| (n as usize) % (N * 8))
     }
 
     pub fn add(&mut self, element: &[u8]) {
@@ -98,10 +57,6 @@ impl<const N: usize> Bloom<N> {
             ones += n.count_ones();
         }
         ones
-    }
-
-    pub fn as_hex(&self) -> String {
-        hex::encode(self.bytes)
     }
 
     fn set_bit(&mut self, index: usize) {
